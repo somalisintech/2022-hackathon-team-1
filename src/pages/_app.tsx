a79ed2a4038cad64e5f3e9d919a5c2b71035a619
Aden <aden.nur@dynabyte.se>
2022-11-05 13:00:32 +0100

    minor changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,18 +6,4 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
 );
 
-GetCampaign(() => {
-  fetch('https://jsonplaceholder.typicode.com/todos')
-  .then(response => response.json())
-  .then(res => setTodos(res.slice(0,10)))
-  .catch(err => setError(err))
-}, [])
-return (
-  <div className="App">
-    {todos.length > 0 ? todos.map((todo:Todo, index: number) => <Todo todo={todo} index={index} handleCompleted={handleCompleted} handleDelete={handleDelete}/>) : (<Loader/>)}
-    <TodoForm handleOnSubmit={handleOnSubmit} />
-  </div>
-);
-}
-
 export default MyApp;
