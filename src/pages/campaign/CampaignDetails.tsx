0d867f6663a58099988f61555f97645d01fe4975
Naima <naimaosman203@gmail.com>
2022-11-06 14:48:30 +0000

    getting form data and consoling it

--- src/pages/campaign/CampaignDetails.tsx
+++ src/pages/campaign/CampaignDetails.tsx
@@ -1,31 +1,26 @@
-import { useEffect } from 'react';
-
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 
-import {
-  getAllCampaigns,
-  getSingleCampaign,
-  postCampaign,
-} from '../../api/campaignAPI';
 import { Background } from '../../background/Background';
-import { Button } from '../../button/Button';
 import { Section } from '../../layout/Section';
 import { NavbarTwoColumns } from '../../navigation/NavbarTwoColumns';
 import { Logo } from '../../templates/Logo';
-import InputFeild from './Input';
 
 const CampaignDetails = () => {
-  useEffect(() => {
-    getAllCampaigns().then((response) => {
-      console.log('getAllCampaigns:', response.data);
-    });
-    getSingleCampaign(1).then((response) => {
-      console.log('GetsingleCmapaign:', response.data);
-    });
-    postCampaign().then((response) => {
-      console.log('PostCmapaign:', response.data);
-    });
-  }, []);
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
+  // useEffect(() => {
+  //   getAllCampaigns().then((response) => {
+  //     console.log('getAllCampaigns:', response.data);
+  //   });
+  //   getSingleCampaign(1).then((response) => {
+  //     console.log('GetsingleCmapaign:', response.data);
+  //   });
+  //   postCampaign().then((response) => {
+  //     console.log('PostCmapaign:', response.data);
+  //   });
+  // }, []);
 
   return (
     <Background color="bg-yellow-100">
@@ -50,25 +45,54 @@ const CampaignDetails = () => {
             Please Enter Your Campaign Details{' '}
           </h1>
         </div>
-        <form className="flex justify-center align-">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex justify-center align-"
+        >
           <div>
             <div>
               <h2>What&#39;s The Campaign Title</h2>
-              <InputFeild />
+              <input
+                {...register('title', { required: true })}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+              />
             </div>
             <br />
             <div>
               <h2>Campaign Description</h2>
-              <textarea name="message" rows={5} cols={30}></textarea>
+              <textarea
+                {...register('description')}
+                name="description"
+                className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                rows={5}
+                cols={30}
+              ></textarea>
             </div>
             <br />
             <div>
               <h2>What&#39;s The Goal Amount?</h2>
-              <InputFeild />
+              <input
+                {...register('amount')}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+              />
             </div>
             <br />
             <div>
-              <h2 className="py-2">Who Are You Raising Funds For?</h2>
+              <h2 {...register('organisation')} className="py-2">
+                Who Are You Raising Funds For?
+              </h2>
               <input type="checkbox" className="border-solid" />
               <label>Myself/Someone else</label>
               <br />
@@ -79,6 +103,11 @@ const CampaignDetails = () => {
             <div>
               <h2>Feature Image</h2>
               <input
+                className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
                 type="image"
                 id="image"
                 alt="Upload img"
@@ -88,22 +117,36 @@ const CampaignDetails = () => {
             <br />
             <div>
               <h2>Add Tagline</h2>
-              <InputFeild />
+              <input
+                {...register('tagline')}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+              />
             </div>
             <br />
             <div>
               <label>Select Category</label>
               <br />
-              <select name="reason" className="px-5">
-                <option value="reason1">Droughts</option>
-                <option value="reason2">Famine</option>
-                <option value="reason3">Sadaqah</option>
+              <select {...register('reason')} name="reason" className="px-5">
+                <option value="Droughts">Droughts</option>
+                <option value="Famine">Famine</option>
+                <option value="Sadaqah">Sadaqah</option>
               </select>
             </div>
             <br />
             <div>
               <h2>Add Location</h2>
-              <InputFeild />
+              <input
+                {...register('location')}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+              />
             </div>
             <br />
             <div>
@@ -117,10 +160,25 @@ const CampaignDetails = () => {
             <br />
             <div>
               <h2>End Date</h2>
-              <InputFeild />
+              <input
+                {...register('endDate')}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+              />
             </div>
             <br />
-            <Button>Submit</Button>
+            <input
+              type="submit"
+              value={'submit'}
+              className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+            />
           </div>
         </form>
       </Section>
